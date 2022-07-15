2c87e2b7f7cab1cc706a6195e8c9e00406c09400
Fabrizio <fabgrel10@gmail.com>
2022-07-14 21:20:49 -0500

    Add responsive menu

--- src/features/sidebar/Sidebar.jsx
+++ src/features/sidebar/Sidebar.jsx
@@ -3,8 +3,24 @@ import Logo from './logo.png';
 import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Sidebar = () => {
+  const toggleMenu = () => {
+    const logo = document.querySelector('.sidebar__logo-container');
+    const menuItems = document.querySelector('.sidebar__items-container');
+    const socialMedia = document.querySelector('.sidebar__social-container');
+    const footer = document.querySelector('.sidebar__footer');
+    logo.classList.toggle('active');
+    menuItems.classList.toggle('active');
+    socialMedia.classList.toggle('active');
+    footer.classList.toggle('active');
+  };
+
   return (
     <div className='sidebar__container'>
+      <a href='/#' className='toggle-button' onClick={toggleMenu}>
+        <span className='toggle-button__line'></span>
+        <span className='toggle-button__line'></span>
+        <span className='toggle-button__line'></span>
+      </a>
       <div className='sidebar__logo-container'>
         <img src={Logo} alt='Logo' />
       </div>
@@ -15,7 +31,7 @@ const Sidebar = () => {
         <li className='sidebar__menu-item'>My Reservations</li>
         <li className='sidebar__menu-item'>Logout</li>
       </ul>
-      <div className='sidebar__social'>
+      <div className='sidebar__social-container'>
         <div className='sidebar__social-item'>
           <FaFacebook size={28} />
         </div>
@@ -23,7 +39,7 @@ const Sidebar = () => {
           <FaTwitter size={28} />
         </div>
         <div className='sidebar__social-item'>
-          <FaInstagram size={28}/>
+          <FaInstagram size={28} />
         </div>
       </div>
       <div className='sidebar__footer'>
