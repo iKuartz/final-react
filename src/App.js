c5a7c55d72986a8623a73902747fcf8f66b7d703
Fabrizio <fabgrel10@gmail.com>
2022-07-14 19:07:45 -0500

    Add initial structure and style to Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Sidebar from './features/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Sidebar />
     </div>
   );
 }
--- src/features/sidebar/Sidebar.jsx
+++ src/features/sidebar/Sidebar.jsx
@@ -1,7 +1,36 @@
+import './Sidebar.scss';
+import Logo from './logo.png';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className='sidebar__container'>
+      <div className='sidebar__logo-container'>
+        <img src={Logo} alt='Logo' />
+      </div>
+      <ul className='sidebar__items-container'>
+        <li className='sidebar__menu-item'>Add Hotel</li>
+        <li className='sidebar__menu-item'>Delete Hotel</li>
+        <li className='sidebar__menu-item'>Reserve Hotel</li>
+        <li className='sidebar__menu-item'>My Reservations</li>
+        <li className='sidebar__menu-item'>Logout</li>
+      </ul>
+      <div className='sidebar__social'>
+        <div className='sidebar__social-item'>
+          <FaFacebook size={28} />
+        </div>
+        <div className='sidebar__social-item'>
+          <FaTwitter size={28} />
+        </div>
+        <div className='sidebar__social-item'>
+          <FaInstagram size={28}/>
+        </div>
+      </div>
+      <div className='sidebar__footer'>
+        <p>Copyright © 2022</p>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
