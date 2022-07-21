3a56d4c0e56baa21a249fa2b6876b5ae21d942a8
Fabrizio <fabgrel10@gmail.com>
2022-07-21 14:14:55 -0500

    Move provider and store to index.js

--- src/App.js
+++ src/App.js
@@ -4,28 +4,24 @@ import Main from './components/main/Main';
 import Sidebar from './components/sidebar/Sidebar';
 import Login from './components/login/login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import store from './redux/configureStore';
-import { Provider } from 'react-redux';
 import PrivateRoute from './routes/PrivateRoute';
 
 function App() {
   return (
     <BrowserRouter>
-      <Provider store={store}>
-        <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route
-            path='/dashboard'
-            element={
-              <PrivateRoute>
-                <div className='app'>
-                  <Sidebar />
-                </div>
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </Provider>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route
+          path='/dashboard'
+          element={
+            <PrivateRoute>
+              <div className='app'>
+                <Sidebar />
+              </div>
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </BrowserRouter>
   );
 }
