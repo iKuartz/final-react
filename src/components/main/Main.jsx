c0b911e9256858089e368ecb846c973320e00d8c
Fabricio Garcia <fabgrel10@gmail.com>
2022-07-27 21:20:45 -0500

    Connect Carousel to API

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -9,43 +9,10 @@ import {
 } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import './Main.scss';
-
-const hotels = [
-  {
-    id: 1,
-    name: 'Papagayo Hotel',
-    image_url: 'https://placehold.jp/3d4070/ffffff/150x150.png',
-    description:
-      'Ideally set in the Centro district of Playa del Carmen, Papagayo Hotel Boutique is located a 3-minute walk from Playa del Carmen Beach.',
-    price: '$200'
-  },
-  {
-    id: 2,
-    name: 'In Fashion Hotel & Spa',
-    image_url: 'https://placehold.jp/3d4070/ffffff/150x150.png',
-    description:
-      'Located in Playa del Carmen, In Fashion Hotel & Spa is 100 m from the Caribbean Sea.1 outdoor pool and a terrace with ocean views. Enjoy the experience!',
-    price: '$300'
-  },
-  {
-    id: 3,
-    name: 'Wyndham Garden ',
-    image_url: 'https://placehold.jp/3d4070/ffffff/150x150.png',
-    description:
-      'This property is a 12-minute walk from the beach. Located in the gated community Playacar in Playa del Carmen, Wyndham Garden Playa del Carmen hotel.',
-    price: '$400'
-  },
-  {
-    id: 4,
-    name: 'Hotel Riviera Del Sol',
-    image_url: 'https://placehold.jp/3d4070/ffffff/150x150.png',
-    description:
-      'This property is 2 minutes walk from the beach. Set in beautiful tropical gardens, Riviera Del Sol is set 3 minutes walk from the beach, on the Mayan Riviera.',
-    price: '$500'
-  }
-];
+import { getHotelsFromApi } from '../../redux/hotels/hotels';
+import { getSession } from '../../storage/session';
 
 function NextArrow(props) {
   const { className, style, onClick } = props;
@@ -70,6 +37,21 @@ function PrevArrow(props) {
 }
 
 const Main = () => {
+  const dispatch = useDispatch();
+  const session = getSession();
+  const navigate = useNavigate();
+  const state = useSelector(store => store.hotels);
+
+  useEffect(() => {
+    dispatch(getHotelsFromApi(5, 0));
+    if (session.token === null) {
+      navigate('/login');
+    }
+  }, []);
+
+  const hotels = state.data;
+  console.log('HOTELS: ', hotels);
+
   const settings = {
     dots: true,
     infinite: false,
@@ -115,12 +97,12 @@ const Main = () => {
           {hotels.map(hotel => (
             <div className='hotel__container' key={hotel.id}>
               <div className='hotel__image-container'>
-                <img src={hotel.image_url} alt={hotel.name} />
+                <img src={hotel.image_path} alt={hotel.name} />
               </div>
               <div>
                 <h4 className='hotel__name'>{hotel.name}</h4>
                 <p className='hotel__description'>{hotel.description}</p>
-                <p className='hotel__price'>{hotel.price} per night</p>
+                <p className='hotel__price'>{hotel.address.country}</p>
               </div>
               <div className='sidebar__social-container'>
                 <div className='sidebar__social-item'>
