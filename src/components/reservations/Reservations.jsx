38eb2cfa592e006f73c59d59c54a888a199b0977
Fabricio Garcia <fabgrel10@gmail.com>
2022-07-28 12:08:24 -0500

    Connect store to Reservations component

--- src/components/reservations/Reservations.jsx
+++ src/components/reservations/Reservations.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { getHotelsFromApi } from '../../redux/hotels/hotels';
+import { getSession } from '../../storage/session';
 
 const Reservations = () => {
-  return <div>Reservations</div>;
+  const dispatch = useDispatch();
+  const session = getSession();
+  const navigate = useNavigate();
+  const state = useSelector(store => store.hotels);
+
+  useEffect(() => {
+    if (session.token === null) {
+      navigate('/login');
+    }
+    dispatch(getHotelsFromApi(5, 0));
+  }, [dispatch, navigate]);
+
+  if (state.data) {
+    const hotels = state.data;
+
+    return (
+      <div className='main__container'>
+        <h2 className='main__title'>Recommended Hotels</h2>
+      </div>
+    );
+  }
 };
 
 export default Reservations;
