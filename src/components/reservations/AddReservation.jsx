b061aad26f727dd6238e197fcb24d23dac123874
Fabricio Garcia <fabgrel10@gmail.com>
2022-07-28 13:24:26 -0500

    Add form for reservations

--- src/components/reservations/AddReservation.jsx
+++ src/components/reservations/AddReservation.jsx
@@ -1,7 +1,28 @@
 const AddReservation = () => {
-  return (
-    <div>AddReservation</div>
-  )
-}
+  const handleOnSubmit = e => {
+    e.preventDefault();
+    console.log('submit');
+  };
 
-export default AddReservation
+  return (
+    <div>
+      <h2>Add Reservation</h2>
+      <form>
+        <label htmlFor='nameContent'>Hotel:</label>
+        <select id='nameContent' name='nameContent'>
+          <option value=''> - Select Hotel - </option>
+          {}
+        </select>
+        <label htmlFor='startDateContent'>Start Date: </label>
+        <input id='startDateContent' name='startDateContent' type='date' />
+        <label htmlFor='endDateContent'>End Date: </label>
+        <input id='endDateContent' name='endDateContent' type='date' />
+        <button type='submit' onClick={handleOnSubmit}>
+          Reserve
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddReservation;
