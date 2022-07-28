ff6390014412d3ec051eb81dabb6e6eee8767e4d
Fabricio Garcia <fabgrel10@gmail.com>
2022-07-28 14:06:08 -0500

    Add style to AddReservation component

--- src/components/reservations/AddReservation.jsx
+++ src/components/reservations/AddReservation.jsx
@@ -1,26 +1,70 @@
+import { useSelector } from 'react-redux';
+import './reservations.scss';
+
 const AddReservation = () => {
+  const state = useSelector(store => store.hotels);
+  let hotelList = [];
+
   const handleOnSubmit = e => {
     e.preventDefault();
     console.log('submit');
   };
 
+  if (state.data) {
+    const hotels = state.data;
+
+    hotelList = hotels.map(house => (
+      <option key={house.id} value={house.id}>
+        {house.name}
+      </option>
+    ));
+  }
+
   return (
-    <div>
-      <h2>Add Reservation</h2>
-      <form>
-        <label htmlFor='nameContent'>Hotel:</label>
-        <select id='nameContent' name='nameContent'>
-          <option value=''> - Select Hotel - </option>
-          {}
-        </select>
-        <label htmlFor='startDateContent'>Start Date: </label>
-        <input id='startDateContent' name='startDateContent' type='date' />
-        <label htmlFor='endDateContent'>End Date: </label>
-        <input id='endDateContent' name='endDateContent' type='date' />
-        <button type='submit' onClick={handleOnSubmit}>
-          Reserve
-        </button>
-      </form>
+    <div className='center-vertical'>
+      <div className='container'>
+        <h2>Add Reservation</h2>
+        <form>
+          <div className='mb-3'>
+            <label htmlFor='nameContent' className='form-label'>
+              Hotel:
+            </label>
+            <select id='nameContent' name='nameContent' className='form-select'>
+              <option value=''> - Select Hotel - </option>
+              {hotelList}
+            </select>
+          </div>
+          <div className='mb-3'>
+            <label htmlFor='startDateContent' className='form-label'>
+              Start Date:{' '}
+            </label>
+            <input
+              id='startDateContent'
+              className='form-control'
+              name='startDateContent'
+              type='date'
+            />
+          </div>
+          <div className='mb-3'>
+            <label htmlFor='endDateContent' className='form-label'>
+              End Date:{' '}
+            </label>
+            <input
+              id='endDateContent'
+              className='form-control'
+              name='endDateContent'
+              type='date'
+            />
+          </div>
+          <button
+            type='submit'
+            className='btn btn-primary'
+            onClick={handleOnSubmit}
+          >
+            Reserve
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
