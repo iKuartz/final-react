3c368d7ce4a0170756f3e10dcf033e563199c210
lisandro seia <lisandroseia11@gmail.com>
2022-07-29 14:57:17 -0300

    Add post hotel logic

--- src/redux/hotels/hotels.js
+++ src/redux/hotels/hotels.js
@@ -1,9 +1,28 @@
 import { getSession } from "../../storage/session"
 import axios from 'axios'
 const GET_HOTELS = 'hotels/GET_HOTELS'
+const POST_HOTEL = 'hotels/POST_HOTEL'
 
 const session = getSession()
 const initialState = []
+
+export const  postHotel = (payload) => ({
+    type: POST_HOTEL,
+    payload
+})
+
+
+const config2 = {
+    headers: {
+      'content-type': 'multipart/form-data',
+      token: session.token
+    },
+  }; 
+
+export const postHotelToApi = (obj) =>{
+    axios.post('http://rails-hotels-api.herokuapp.com/v1/hotels', obj, config2)
+    .then((data) => console.log(data))
+}
 
 export const getHotels = (payload) => ({
      type: GET_HOTELS,
