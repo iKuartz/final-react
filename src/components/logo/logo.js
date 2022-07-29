c130225f8aeedadf622544b82c36a3f126bc6781
lisandro seia <lisandroseia11@gmail.com>
2022-07-29 18:06:24 -0300

    Add link to hotel

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,11 +1,13 @@
 import React from "react"
 import logo from '../../assets/images/logo.png'
 import './logo.scss'
+import { useNavigate } from "react-router-dom"
 
 
 const Logo = () => {
+  const navigate = useNavigate()
   return(
-    <div className='logo'>
+    <div className='logo' onClick={() => navigate('/')}>
     <img src={logo} alt='logo' />
     <h1 unselectable="on">Hotel</h1>
   </div>
