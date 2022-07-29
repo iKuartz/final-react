cdf0060b212fea9ea78ad92d5cf473bc67c3e723
Fabricio Garcia <fabgrel10@gmail.com>
2022-07-29 17:42:13 -0500

    Connect hotels reducer to external API

--- src/redux/hotels/hotels.js
+++ src/redux/hotels/hotels.js
@@ -1,39 +1,41 @@
-import { getSession } from "../../storage/session"
-import axios from 'axios'
-const GET_HOTELS = 'hotels/GET_HOTELS'
+import { getSession } from '../../storage/session';
+import axios from 'axios';
+const GET_HOTELS = 'hotels/GET_HOTELS';
 
-const session = getSession()
-const initialState = []
+const session = getSession();
+const initialState = [];
 
-export const getHotels = (payload) => ({
-     type: GET_HOTELS,
-     payload
-})
+export const getHotels = payload => ({
+  type: GET_HOTELS,
+  payload
+});
 
 const config = {
-    headers: {
-        "Content-type":"application/json",
-        'token': session.token
+  headers: {
+    'Content-type': 'application/json',
+    token: session.token
+  }
+};
+
+export const getHotelsFromApi = (amount, index) => dispatch => {
+  axios
+    .get(
+      `https://rails-hotels-api.herokuapp.com/v1/hotels?limit=${amount}&offset=${index}`,
+      config
+    )
+    .then(data => dispatch(getHotels(data.data)))
+    .catch(error => console.log(error));
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_HOTELS: {
+      return action.payload;
     }
-}
-
-export const getHotelsFromApi = (amount, index) => (dispatch) => {
-    
-     axios.get(`http://localhost:3000/v1/hotels?limit=${amount}&offset=${index}`, config)
-    .then((data) => dispatch(getHotels(data.data)))
-    .catch((error) => console.log(error))
-}
-
-const reducer = ( state = initialState, action ) => {
-
-    switch(action.type){
-        case GET_HOTELS:{
-            return action.payload
-        }
-        default: {
-            return state
-        }
+    default: {
+      return state;
     }
-}
+  }
+};
 
 export default reducer;
